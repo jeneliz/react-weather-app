e20202b10420143fd3b1ffafbe96cab97dde0aad
jeneliz <109545820+jeneliz@users.noreply.github.com>
2023-02-23 12:57:32 -0800

    added GitHub and portfolio url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://coding-on-coffee.netlify.app/" target="_blank">
+          Jennifer Brown
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/jeneliz/react-weather-app" target="_blank">
+          opened sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
